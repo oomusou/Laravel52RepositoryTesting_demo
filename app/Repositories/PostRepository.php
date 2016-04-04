a06819d67f0899e053012a787df81d72edce6865
oomusou <oomusou@gmail.com>
2016-04-04 23:26:09 +0800

    回傳collection

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -2,9 +2,18 @@
 
 namespace App\Repositories;
 
+use App\Post;
+use Illuminate\Database\Eloquent\Collection;
+
 class PostRepository
 {
+    /**
+     * @return Collection
+     */
     public function getLatest3Posts()
     {
+        return Post::orderBy('id', 'desc')
+            ->take(3)
+            ->get();
     }
 }
