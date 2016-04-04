dd72b8eac931c304c7aef8edc506b890dbd369d1
oomusou <oomusou@gmail.com>
2016-04-04 22:09:19 +0800

    安裝IDE Helper

--- config/app.php
+++ config/app.php
@@ -148,7 +148,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Mpociot\LaravelTestFactoryHelper\TestFactoryHelperServiceProvider::class,
-
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
